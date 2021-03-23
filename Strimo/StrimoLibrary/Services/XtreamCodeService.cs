f79867672e83c59ee5e26758d1c969365a19e70b
decaf-programmer <decaf.programmer@gmail.com>
2021-03-24 01:19:15 +0800

    Add UserModel and Xtream Code Service into Library for Authentication.

---
+++ Strimo/StrimoLibrary/Services/XtreamCodeService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrimoLibrary.Services
+{
+    public static class XtreamCodeService
+    {
+        public static string server_url = "http://hd.qweret.com:80";
+
+        public static string Auth(string username, string password)
+        {
+            var request_url = $"{server_url}/player_api.php?username={username}&password={password}";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(request_url);
+
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException e)
+            {
+                if (e.Status == WebExceptionStatus.ProtocolError)
+                {
+                    return "Bad_Streaming_URL";
+                }
+                return "Error";
+            }
+
+        }
+    }
+}
+}
