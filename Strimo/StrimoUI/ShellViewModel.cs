f3a2dc72f68513b09f49f3fff834baef57978577
decaf-programmer <decaf.programmer@gmail.com>
2021-03-23 23:10:59 +0800

    Implement the structure of the Bootstrapper in the Application

---
+++ Strimo/StrimoUI/ShellViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrimoUI
+{
+    public class ShellViewModel:Conductor<Screen>.Collection.OneActive
+    {
+        private readonly IEventAggregator eventAggregator;
+
+        public ShellViewModel(IEventAggregator _eventAggregaotr)
+        {
+            eventAggregator = _eventAggregaotr;
+            
+            Items.AddRange(new Screen[] { });
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+
+            eventAggregator.Subscribe(this);
+            //ActivateItem();
+        }
+
+        protected override void OnDeactivate(bool close)
+        {
+            base.OnDeactivate(close);
+            eventAggregator.Unsubscribe(this);
+        }
+    }
+}
