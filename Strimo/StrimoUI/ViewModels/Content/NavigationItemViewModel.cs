99f770954b242c1e4863b16c51fa39537700c779
Decaf <decaf.programmer@gmail.com>
2021-03-28 04:10:29 +0800

    DataBinding in Visibility in C# WPF MVVM Reaction for listview and expander data.

--- Strimo/StrimoUI/ViewModels/Content/NavigationItemViewModel.cs
+++ Strimo/StrimoUI/ViewModels/Content/NavigationItemViewModel.cs
@@ -9,7 +9,7 @@ using System.Windows.Controls;
 
 namespace StrimoUI.ViewModels.Content
 {
-    public class NavigationItemViewModel:Screen
+    public class NavigationItemViewModel : Screen
     {
         private string _header;
         public string Header
@@ -17,7 +17,7 @@ namespace StrimoUI.ViewModels.Content
             get { return _header; }
             set
             {
-                if(_header == value)
+                if (_header == value)
                 {
                     return;
                 }
@@ -42,12 +42,12 @@ namespace StrimoUI.ViewModels.Content
         }
 
         private List<SubItemModel> _subItems;
-        public List<SubItemModel> SubItems{
-            get{
+        public List<SubItemModel> SubItems {
+            get {
                 return _subItems;
             }
-            set{
-                if(_subItems == value){
+            set {
+                if (_subItems == value) {
                     return;
                 }
 
@@ -57,13 +57,13 @@ namespace StrimoUI.ViewModels.Content
         }
 
         private UserControl _screen;
-        public UserControl Screen 
-        { 
-            get{
+        public UserControl Screen
+        {
+            get {
                 return _screen;
             }
-            set{
-                if(_screen == value){
+            set {
+                if (_screen == value) {
                     return;
                 }
                 _screen = value;
@@ -71,16 +71,45 @@ namespace StrimoUI.ViewModels.Content
             }
         }
 
-
-        public NavigationItemViewModel()
+        private bool _listViewItemVisible;
+        public bool ListViewItemVisible
         {
+            get
+            {
+                return _listViewItemVisible;
+            }
+            set
+            {
+                _listViewItemVisible = value;
+                NotifyOfPropertyChange(() => ListViewItemVisible);
+            }
         }
+
+
+        private bool _expanderVisibile;
+        public bool ExpanderVisible
+        {
+            get
+            {
+                return _expanderVisibile;
+            }
+            set
+            {
+                _expanderVisibile = value;
+                NotifyOfPropertyChange(() => ExpanderVisible);
+            }
+        }
+
+
 
         public NavigationItemViewModel(string header, string imageName, List<SubItemModel> subItems, UserControl screen){
             Header = header;
             ImageName = imageName;
             SubItems = subItems;
             Screen = screen;
+
+            ExpanderVisible = SubItems == null ? false : true;
+            ListViewItemVisible = SubItems == null ? true : false;
         }
     }
 }
