fd7278a1f6870288bf41263a930abb1b9bf8f395
Decaf <decaf.programmer@gmail.com>
2021-04-07 02:55:31 +0800

    Register Database and Table and User strucutre.

---
+++ Strimo/StrimoLibrary/Models/UserDBModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrimoLibrary.Models
+{
+    public class UserDBModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public bool loginStatus { get; set; }
+        public string lastLoginDate { get; set; }
+    }
+}
